546908f5c730944afff4b8cdc656e5b96499f059
jackclayton247 <130365662+jackclayton247@users.noreply.github.com>
2023-06-11 21:26:09 +0100

    Update sprites.py

--- Dungeon Game/sprites.py
+++ Dungeon Game/sprites.py
@@ -1,78 +1,99 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      TWY-6451
-#
-# Created:     25/05/2023
-# Copyright:   (c) TWY-6451 2023
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+#sprites
 
 import pygame as pg
 from settings import *
-
+vec = pg.math.Vector2
 
 class Player(pg.sprite.Sprite):
-    def __init__(self, game, x, y):
+    def __init__(self, game, x, y):    #sprite image
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(YELLOW)
+        self.image = game.player_img
         self.rect = self.image.get_rect()
-        self.vx, self.vy = 0, 0
-        self.x = x * TILESIZE
-        self.y = y * TILESIZE
-
+        self.vel = vec(0, 0)
+        self.pos = vec(x, y) * TILESIZE
+    
     def get_keys(self):
-        self.vx, self.vy = 0, 0
+        self.vel = vec(0, 0)   #catches movement events
         keys =pg.key.get_pressed()
         if keys[pg.K_LEFT] or keys[pg.K_a]:
-            self.vx = -PLAYER_SPEED
+            self.vel.x = -PLAYER_SPEED
         if keys[pg.K_RIGHT] or keys[pg.K_d]:
-            self.vx = PLAYER_SPEED
+            self.vel.x = PLAYER_SPEED
         if keys[pg.K_UP] or keys[pg.K_w]:
-            self.vy = -PLAYER_SPEED
+            self.vel.y = -PLAYER_SPEED
         if keys[pg.K_DOWN] or keys[pg.K_s]:
-            self.vy = PLAYER_SPEED
-        if self.vx != 0 and self.vy != 0:
-            self.vx *= 0.7071
-            self.vy *= 0.7071
+            self.vel.y = PLAYER_SPEED
+        if self.vel.x != 0 and self.vel.y != 0:
+            self.vel *= 0.7071
 
-    def collide_with_walls(self, dir):
+    def collide_with_walls_player(self, dir):
         if dir == "x":
             hits = pg.sprite.spritecollide(self, self.game.walls, False)
             if hits:
-                if self.vx > 0:
-                    self.x = hits[0].rect.left - self.rect.width
-                if self.vx < 0:
-                    self.x = hits[0].rect.right
-            self.vx = 0
-            self.rect.x = self.x
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.rect.width
+                    self.game.gun.pos.x = self.pos.x + 64
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right
+                    self.game.gun.pos.x = self.pos.x + 64
+            self.vel.x = 0
+            self.rect.x = self.pos.x
         if dir == "y":
             hits = pg.sprite.spritecollide(self, self.game.walls, False)
             if hits:
-                if self.vy > 0:
-                    self.y = hits[0].rect.top - self.rect.height
-                if self.vy < 0:
-                    self.y = hits[0].rect.bottom
-            self.vy = 0
-            self.rect.y = self.y
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.rect.height
+                    self.game.gun.pos.y = self.pos.y + 64
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom
+                    self.game.gun.pos.y = self.pos.y + 64
+            self.vel.y = 0
+            self.rect.y = self.pos.y
 
     def update(self):
         self.get_keys()
-        self.x += self.vx * self.game.dt
-        self.y += self.vy * self.game.dt
-        self.rect.x =self.x
-        self.collide_with_walls("x")
-        self.rect.y =self.y
-        self.collide_with_walls("y")
+        self.pos += self.vel * self.game.dt
+        self.rect.x =self.pos.x
+        self.collide_with_walls_player("x")
+        self.rect.y =self.pos.y
+        self.collide_with_walls_player("y")
 
+class Gun(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites,    
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game 
+        self.image = game.gun_img
+        self.rect = self.image.get_rect()
+        self.coord = vec(0,0)
+        self.pos = vec(x+1, y+1) * TILESIZE
+        print(self.pos)
+    
+    def get_keys(self):
+        self.coord = vec(0, 0)   #catches movement events
+        keys =pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.coord.x = -PLAYER_SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.coord.x = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.coord.y = -PLAYER_SPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.coord.y = PLAYER_SPEED
+        if self.coord.x != 0 and self.coord.y != 0:
+            self.coord *= 0.7071
+
+    def update(self):
+        self.get_keys()
+        self.pos += self.coord * self.game.dt
+        self.rect.x =self.pos.x
+        self.rect.y =self.pos.y
 
 class Wall(pg.sprite.Sprite):
-    def __init__(self, game, x, y):
-        self.groups = game.all_sprites, game.walls
+    def __init__(self, game, x, y):   #sprite image
+        self.groups = game.all_sprites, game.walls    
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
         self.image = pg.Surface((TILESIZE, TILESIZE))
