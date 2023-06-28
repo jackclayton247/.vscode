901b37fce3fd5947884c56b0ff10b38889348cdc
jackclayton247 <130365662+jackclayton247@users.noreply.github.com>
2023-06-28 19:57:44 +0100

    Update sprites.py

--- Dungeon Game/sprites.py
+++ Dungeon Game/sprites.py
@@ -1,165 +1,140 @@
-#sprites
+#Dungeon Crawler
 
 import pygame as pg
-from settings import *
-import math
+import sys
 from os import path
-vec = pg.math.Vector2
-from random import uniform
+from settings import *
+from sprites import *
+from tilemap import *
 
-class Player(pg.sprite.Sprite):
-    def __init__(self, game, x, y):    #sprite image
-        self.groups = game.all_sprites
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        self.image = game.player_img
-        self.rect = self.image.get_rect()
-        self.vel = vec(0, 0)
-        self.pos = vec(x, y) * TILESIZE
+class Game:
+    def __init__(self): #creates screen
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        self.load_data()
 
-    def get_keys(self):
-        self.vel = vec(0, 0)   #catches movement events
-        keys =pg.key.get_pressed()
-        if keys[pg.K_LEFT] or keys[pg.K_a]:
-            self.vel.x = -PLAYER_SPEED
-        if keys[pg.K_RIGHT] or keys[pg.K_d]:
-            self.vel.x = PLAYER_SPEED
-        if keys[pg.K_UP] or keys[pg.K_w]:
-            self.vel.y = -PLAYER_SPEED
-        if keys[pg.K_DOWN] or keys[pg.K_s]:
-            self.vel.y = PLAYER_SPEED
-        if self.vel.x != 0 and self.vel.y != 0:
-            self.vel *= 0.7071
+    def load_data(self): #loads player graphics
+        game_folder = path.dirname(__file__)
+        img_folder = path.join(game_folder, 'img')
+        self.map = Map(path.join(game_folder, 'map.txt'))
+        self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
+        self.gun_img = pg.transform.rotozoom(pg.image.load(path.join(img_folder, GUN_IMG)).convert_alpha(), 0, 1.5)
+        self.bullet_img = pg.transform.rotozoom(pg.image.load(path.join(img_folder, BULLET_IMG)).convert_alpha(), 0 , 0.7)
 
-    def collide_with_walls_player(self, dir):
-        if dir == "x":
-            hits = pg.sprite.spritecollide(self, self.game.walls, False)
-            if hits:
-                if self.vel.x > 0: #check direction of collision and stops that movement
-                    self.pos.x = hits[0].rect.left - self.rect.width
-                    self.game.gun.pos.x = self.pos.x + 64
-                if self.vel.x < 0:
-                    self.pos.x = hits[0].rect.right
-                    self.game.gun.pos.x = self.pos.x + 64
-            self.vel.x = 0
-            self.rect.x = self.pos.x
-        if dir == "y":
-            hits = pg.sprite.spritecollide(self, self.game.walls, False)
-            if hits:
-                if self.vel.y > 0:  #check direction of collision and stops that movement
-                    self.pos.y = hits[0].rect.top - self.rect.height
-                    self.game.gun.pos.y = self.pos.y + 64
-                if self.vel.y < 0:
-                    self.pos.y = hits[0].rect.bottom
-                    self.game.gun.pos.y = self.pos.y + 64
-            self.vel.y = 0
-            self.rect.y = self.pos.y
+    def new(self):     
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.bullets = pg.sprite.Group()
+        for row, tiles in enumerate(self.map.data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == '2':
+                    self.player = Player(self, col, row) #spawns player
+                    self.gun = Gun(self, col, row) #spawns weapon
+        self.camera = Camera(self.map.width, self.map.height)
 
-    def update(self): #calls all the functions
-        self.get_keys()
-        self.pos += self.vel * self.game.dt
-        self.rect.x =self.pos.x
-        self.collide_with_walls_player("x")
-        self.rect.y =self.pos.y
-        self.collide_with_walls_player("y")
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
 
-class Gun(pg.sprite.Sprite): #weapon image
-    def __init__(self, game, x, y):
-        self.pos = vec(x+0.7, y+1.2) * TILESIZE
-        self.groups = game.all_sprites,
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        self.image = game.gun_img
-        self.base_gun_image = self.image
-        self.hitbox_rect = self.base_gun_image.get_rect(center = self.pos)
-        self.rect = self.hitbox_rect.copy()
-        self.coord = vec(0,0)
-        self.offset = vec(80, 0)
-        self.last_shot = 0
-        self.rot = 0
-        
-
-    def gun_rotation(self): #roates the gun sprite to point the cursor
-        self.mouse_coords = self.pos - [549.5, 441.5] + pg.mouse.get_pos()
-        self.x_change_mouse_gun = (self.mouse_coords[0] - self.hitbox_rect.centerx)
-        self.y_change_mouse_gun = (self.mouse_coords[1] - self.hitbox_rect.centery)
-        self.angle = math.degrees(math.atan2(self.y_change_mouse_gun, self.x_change_mouse_gun))
-        self.image = pg.transform.rotate(self.base_gun_image, -self.angle)
-        offset_rotated = self.offset.rotate(self.angle)
-        self.rect = self.image.get_rect(center = self.pos+offset_rotated)
-
-    def get_keys(self):
-        self.coord = vec(0, 0)   #catches movement events
-        keys =pg.key.get_pressed()
-        if keys[pg.K_LEFT] or keys[pg.K_a]:
-            self.coord.x = -PLAYER_SPEED
-            self.hitbox_rect.center = self.pos
-            self.rect.center = self.hitbox_rect.center
-        if keys[pg.K_RIGHT] or keys[pg.K_d]:
-            self.coord.x = PLAYER_SPEED
-            self.hitbox_rect.center = self.pos
-            self.rect.center = self.hitbox_rect.center
-        if keys[pg.K_UP] or keys[pg.K_w]:
-            self.coord.y = -PLAYER_SPEED
-            self.hitbox_rect.center = self.pos
-            self.rect.center = self.hitbox_rect.center
-        if keys[pg.K_DOWN] or keys[pg.K_s]:
-            self.coord.y = PLAYER_SPEED
-            self.hitbox_rect.center = self.pos
-            self.rect.center = self.hitbox_rect.center
-        if self.coord.x != 0 and self.coord.y != 0:
-            self.coord *= 0.7071
-        if keys[pg.K_SPACE]:
-            now = pg.time.get_ticks()
-            if now - self.last_shot > BULLET_RATE:
-                self.last_shot = now
-                dir = vec(1, 0).rotate(-self.rot)
-                pos = self.pos + BARREL_OFFSET.rotate(-self.rot)
-                Bullet(self.game, pos, dir)
-                self.vel = vec(-KICKBACK, 0).rotate(-self.rot)
-            
-    def update(self):  #calls all the functions
-        self.get_keys()
-        self.gun_rotation()
-        self.pos += self.coord * self.game.dt
-
-class Bullet(pg.sprite.Sprite):
-    def __init__(self, game, pos, dir):
-        self.groups = game.all_sprites, game.bullets
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        self.image = game.bullet_img
-        self.rect = self.image.get_rect()
-        self.pos = vec(pos)
-        self.rect.center = pos
-        #spread = uniform(-GUN_SPREAD, GUN_SPREAD)
-        self.vel = [self.game.gun.x_change_mouse_gun / 100, self.game.gun.y_change_mouse_gun / 100]
-        self.spawn_time = pg.time.get_ticks()
-        self.base_bullet_image = self.image
-    
-    def angle_bullet(self):
-        self.image = pg.transform.rotate(self.base_bullet_image, -self.game.gun.angle)
-        self.rect = self.image.get_rect(center = self.pos)
+    def quit(self):
+        pg.quit()  #enables you to close tab
+        sys.exit()
 
     def update(self):
-        self.pos += (self.vel)
-        self.angle_bullet()
-        self.rect.center = self.pos
-        if pg.sprite.spritecollideany(self, self.game.walls):
-            self.kill()
-        if pg.time.get_ticks() - self.spawn_time > BULLET_LIFETIME:
-            self.kill()
-            print(self.game.gun.angle)
+        # update portion of the game loop
+        self.all_sprites.update()
+        self.camera.update(self.player)
 
-class Wall(pg.sprite.Sprite):
-    def __init__(self, game, x, y):   #sprite image
-        self.groups = game.all_sprites, game.walls
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
-        self.rect.x = x * TILESIZE
-        self.rect.y = y * TILESIZE
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+
+        pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
+    def run(self):
+
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
