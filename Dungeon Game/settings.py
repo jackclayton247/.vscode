0fa2f3738a7ad6b7411c96f10db4fe374aef6b3d
jackclayton247 <130365662+jackclayton247@users.noreply.github.com>
2023-07-05 21:09:01 +0100

    Update settings.py

--- Dungeon Game/settings.py
+++ Dungeon Game/settings.py
@@ -11,6 +11,7 @@ LIGHTGREY = (100, 100, 100)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
 
 #game settings
 WIDTH = 1024    # 16 * 64 or 32 * 32 or 64 * 16
@@ -24,21 +25,30 @@ GRIDWIDTH = WIDTH / TILESIZE
 GRIDHEIGHT = HEIGHT / TILESIZE
 
 #player setting
-PLAYER_SPEED = 500
-PLAYER_IMG = "test2.png"
+PLAYER_SPEED = 300
+PLAYER_IMG = "player.png"
 PLAYER_HIT_RECT = pg.Rect(0, 0, 70, 110)
+PLAYER_LIVES = 6
+HEART_IMG = "heart.png"
+HALF_HEART_IMG = "half heart.png"
+I_FRAMES = 300
+PLAYER_CENTER = [550, 450]
+
 #gun settings
 GUN_IMG = "gun.png"
-GUN_HIT_RECT = pg.Rect(0, 0, 35, 35)
+GUN_HIT_RECT = pg.Rect(0, 0, 0, 0)
 #bullet settings
 BULLET_IMG = "bullet.png"
-BARREL_OFFSET = vec(30, 10)
+BARREL_OFFSET = vec(10, 10)
 BULLET_SPEED = 20
 BULLET_LIFETIME = 100000
 BULLET_RATE = 200
 KICKBACK = 0
 GUN_SPREAD = 5
+BULLET_DAMAGE = 10
 #mob settings
 MOB_IMG = "mob.png"
-MOB_SPEED = 150
-MOB_HIT_RECT = pg.Rect(0, 0, 30, 30)
+MOB_SPEED = 100
+MOB_HIT_RECT = pg.Rect(0, 0, 60, 110)
+MOB_HEALTH = 100
+MOB_DAMAGE = 5
