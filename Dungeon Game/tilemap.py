83013eef9f21dba90a119ca20bdf2f9c4f37e369
jackclayton247 <130365662+jackclayton247@users.noreply.github.com>
2023-09-05 18:03:11 +0100

    Update tilemap.py

--- Dungeon Game/tilemap.py
+++ Dungeon Game/tilemap.py
@@ -11,11 +11,11 @@ class Map:
         with open(filename, 'rt') as f:
             for line in f:
                 self.data.append(line.strip())
-
         self.tilewidth = len(self.data[0])
         self.tileheight = len(self.data)
-        self.width = self.tilewidth * TILESIZE
         self.height = self.tileheight * TILESIZE
+        self.width  = self.tilewidth * TILESIZE
+
 
 class TiledMap:
     def __init__(self, filename):
@@ -23,6 +23,8 @@ class TiledMap:
         self.width = tm.width * tm.tilewidth
         self.height = tm.height * tm.tileheight
         self.tmxdata = tm
+    def get_width(self):
+        self.width = tm.width * tm.tilewidth
 
     def render(self, surface):
         ti = self.tmxdata.get_tile_image_by_gid
@@ -44,10 +46,9 @@ class Camera:
         self.camera = pg.Rect(0, 0, width, height)
         self.width = width
         self.height = height
-        player_offset = False
 
     def apply(self, entity):
-        return entity.rect.move(self.camera.topleft)
+        return entity.rect.move(self.camera.topleft) 
 
     def apply_rect(self, rect):
         return rect.move(self.camera.topleft)
@@ -57,12 +58,10 @@ class Camera:
         x = -target.rect.x + int(WIDTH / 2)
         y = -target.rect.y + int(HEIGHT / 2)
         self.camera = pg.Rect(x, y, self.width, self.height)
-        if x == -(self.width - WIDTH):
-            self.game.gun.player_offset = False
 
-        # limit scrolling to map size
-        x = min(0, x)  # left
-        y = min(0, y)  # top
-        x = max(-(self.width - WIDTH), x)  # right
-        y = max(-(self.height - HEIGHT), y)  # bottom
-        self.camera = pg.Rect(x, y, self.width, self.height)
+    # limit scrolling to map size
+        x = min(0 + (75/2), x)  # left
+        y = min(0 + (55), y)  # top
+        x = max(-(self.width - WIDTH) + (75/2), x )  # right
+        y = max(-(self.height - HEIGHT) + (55), y )  # bottom
+        self.camera = pg.Rect(x - (75/2), y - (55), self.width, self.height)
